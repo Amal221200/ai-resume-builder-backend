9bd89807205883ed29234e9909a278f519b49244
Amal221200 <amalmurikkoli2000@gmail.com>
2024-06-22 10:41:36 +0530

    netlify.toml

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,15 +2,15 @@ export default () => ({
     "netlify-deployments": {
         enabled: true,
         config: {
-            accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN
+            accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+            sites: [
+                {
+                    name: "ai-resume-builder-backend",
+                    id: "2b07ff05-cbc6-4c82-9de3-acec85a70069",
+                    buildHook: "https://api.netlify.com/build_hooks/6676509d3bf94c31184ac55f",
+                    branch: 'master' // optional
+                }
+            ],
         },
-        sites: [
-            {
-                name: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_SITE_NAME,
-                id: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_SITE_ID,
-                buildHook:process.env.NETLIFY_DEPLOYMENTS_PLUGIN_BUILD_HOOK,
-                branch: 'master' // optional
-            }
-        ],
     },
 });
