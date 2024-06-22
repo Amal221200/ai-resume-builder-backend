ed7e7dc78d98e03cf23415545a30a11231fb6f24
Amal221200 <amalmurikkoli2000@gmail.com>
2024-06-22 10:00:51 +0530

    netlify functions

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default () => ({
+    "netlify-deployments": {
+        enabled: true,
+        config: {
+            accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN
+        },
+        sites: [
+            {
+                name: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_SITE_NAME,
+                id: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_SITE_ID,
+                buildHook:process.env.NETLIFY_DEPLOYMENTS_PLUGIN_BUILD_HOOK,
+                branch: 'master' // optional
+            }
+        ],
+    },
+});
